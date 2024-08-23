8d4b487bd08cd88365931be326b51ef8a9803681
x54x6fx6dx6dx79 <tom31schule@web.de>
2024-08-23 22:38:38 +0200

    asd

--- blog/src/app/layout.tsx
+++ blog/src/app/layout.tsx
@@ -42,6 +42,31 @@ export default function RootLayout({
         <script type="text/javascript" src="https://assets.calendly.com/assets/external/widget.js" async></script>
       </head>
       <body className={cn("h-fit flex flex-col", inter.className)}>
+      <script
+            dangerouslySetInnerHTML={{
+              __html: `
+                !function(f,b,e,v,n,t,s)
+                {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+                n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+                if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+                n.queue=[];t=b.createElement(e);t.async=!0;
+                t.src=v;s=b.getElementsByTagName(e)[0];
+                s.parentNode.insertBefore(t,s)}(window, document,'script',
+                'https://connect.facebook.net/en_US/fbevents.js');
+                fbq('init', '1051403233002980');
+                fbq('track', 'PageView');
+              `,
+            }}
+          />
+          <noscript>
+            <img
+              height="1"
+              width="1"
+              style={{ display: 'none' }}
+              src="https://www.facebook.com/tr?id=1051403233002980&ev=PageView&noscript=1"
+              alt="Facebook Pixel"
+            />
+          </noscript>
         <Navbar links={Links}/>
         {children}
         <BackToTop/>
